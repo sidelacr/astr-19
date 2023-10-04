aeb48ed960abe715b1d8b5b45e0aaf34be9a130d
Sierra <146883186+sidelacr@users.noreply.github.com>
2023-10-04 13:35:00 -0700

    Session 1 Prompt for Coding Journal 1

---
+++ intro.py
@@ -0,0 +1,8 @@
+def PrintHelloImSierra():
+	print("Hello I'm Sierra\nMy favorite movie is Cars and I love to eat chocolate")
+
+def main():
+	PrintHelloImSierra()
+
+if __name__=="__main__":
+	main()
