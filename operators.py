62c6dca2a4078da1ead7fee327106b6dcbc7b72d
Sierra <146883186+sidelacr@users.noreply.github.com>
2023-10-05 13:04:25 -0700

    operators (assignment and comparison)
    in class exercise October 5th

---
+++ operators.py
@@ -0,0 +1,36 @@
+x = 9	#define an integer x equal to 9
+y = 3	#define an integer y equal to 3
+
+# Arithmetic Operators
+print("x+y = ", x+y)		#Addition 
+print("x-y = ", x-y)		#Subtraction
+print("x*y = ", x*y)		#Multiplication
+print("x/y = ", x/y)		#Division
+print("x%y = ", x%y)		#Modculus
+print("x**y = ", x**y)	#Exponentiation
+x = 9.191823
+print(x//y)				#Floor division
+
+# Assignment Operators
+x = 9 		# set x = 9 
+x +=3 		# x = x + 3
+print("x = 9; x+=3; x =",x)
+x = 9 		# set x = 9
+x -=3 		# x = x - 3
+print("x = 9; x-=3; x = ",x)
+x *=3 		# x = x * 3
+print("x *= 3; x = ",x)
+x /=3 		# x = x / 3
+print("x /= 3; x = ",x)
+x **= 3 	# x = x ** 3
+print("x **= 3; x = ",x)
+
+# Comparison Operators
+x = 9
+y = 3
+print("x==y ?", x==y) #True if x equals y, False otherwise
+print("x!=y ?", x!=y) #True if x does not equal y, False otherwise
+print("x>y ?", x>y)   #True if x is greater than y, False otherwise
+print("x<y ?", x<y)   #True if x is less than y, False otherwise
+print("x>=y ?", x>=y) #True is x is greater than or ewual ot y, False otherwise
+print("x<=y ?", x<=y) #True if x is less than or equal to y, False otherwise 
