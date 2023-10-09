4eadf4f803cccd53a783bf4d6ce0a29dc7ba8f2c
Sierra <146883186+sidelacr@users.noreply.github.com>
2023-10-09 12:56:21 -0700

    Session 2 prompt for Coding Journal 1

---
+++ 1-2.py
@@ -0,0 +1,55 @@
+ # Sum of Two Floating Point Numbers
+def PrintSumFloat():
+	print("Sum of Two Floating Point Numbers")
+
+def main():
+	PrintSumFloat()
+
+if __name__=="__main__":
+	main()
+x = 2.5
+y = 3.5
+print(float(2.5)) #define an floaitng point number as 2.5
+print(float(3.5)) #define an floating point number as 3.5
+
+print("x+y = ", x+y) #equation for sum of floating point numbers
+
+# Difference between Two Integers
+def PrintDiffInt():
+	print("Difference between Two Integers")
+
+def main():
+	PrintDiffInt()
+
+if __name__=="__main__":
+	main()
+i = 5
+print(i)
+
+print(type(i))
+
+t = 8
+print(t)
+
+print(type(t))
+
+print("i-t = ", i-t) #equation for difference between two integers
+
+# Product of a Floating Point Number and an Integer
+def PrintProdFloatInt():
+	print("Product of a Floating Point Number and an Integer")
+
+def main():
+	PrintProdFloatInt()
+
+if __name__=="__main__":
+	main()
+
+i = 5
+print(i)
+print(type(i))
+
+x = 2.5
+print(float(2.5))
+
+print("x*i = ", x*i)
